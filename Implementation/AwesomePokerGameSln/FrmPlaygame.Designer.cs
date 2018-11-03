0cd1a0f171d0421ed2ef61b2be0bb984687ba287
BreonT <bjt020@latech.edu>
2018-11-03 18:32:39 -0500

    Add files via upload

---
+++ Implementation/AwesomePokerGameSln/FrmPlaygame.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace AwesomePokerGameSln {
+  partial class FrmPlaygame {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+            this.picCard1 = new System.Windows.Forms.PictureBox();
+            this.picCard3 = new System.Windows.Forms.PictureBox();
+            this.picCard4 = new System.Windows.Forms.PictureBox();
+            this.picCard5 = new System.Windows.Forms.PictureBox();
+            this.picCard2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblHandType = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.Highscore = new System.Windows.Forms.Label();
+            this.Dlhandtype = new System.Windows.Forms.Label();
+            this.handwinloss = new System.Windows.Forms.Label();
+            this.Wallet = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.Bet = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCard1
+            // 
+            this.picCard1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picCard1.Location = new System.Drawing.Point(101, 225);
+            this.picCard1.Margin = new System.Windows.Forms.Padding(4);
+            this.picCard1.Name = "picCard1";
+            this.picCard1.Size = new System.Drawing.Size(149, 148);
+            this.picCard1.TabIndex = 0;
+            this.picCard1.TabStop = false;
+            // 
+            // picCard3
+            // 
+            this.picCard3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picCard3.Location = new System.Drawing.Point(460, 225);
+            this.picCard3.Margin = new System.Windows.Forms.Padding(4);
+            this.picCard3.Name = "picCard3";
+            this.picCard3.Size = new System.Drawing.Size(149, 148);
+            this.picCard3.TabIndex = 1;
+            this.picCard3.TabStop = false;
+            // 
+            // picCard4
+            // 
+            this.picCard4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picCard4.Location = new System.Drawing.Point(640, 225);
+            this.picCard4.Margin = new System.Windows.Forms.Padding(4);
+            this.picCard4.Name = "picCard4";
+            this.picCard4.Size = new System.Drawing.Size(149, 148);
+            this.picCard4.TabIndex = 2;
+            this.picCard4.TabStop = false;
+            // 
+            // picCard5
+            // 
+            this.picCard5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picCard5.Location = new System.Drawing.Point(825, 225);
+            this.picCard5.Margin = new System.Windows.Forms.Padding(4);
+            this.picCard5.Name = "picCard5";
+            this.picCard5.Size = new System.Drawing.Size(149, 148);
+            this.picCard5.TabIndex = 3;
+            this.picCard5.TabStop = false;
+            // 
+            // picCard2
+            // 
+            this.picCard2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picCard2.Location = new System.Drawing.Point(281, 225);
+            this.picCard2.Margin = new System.Windows.Forms.Padding(4);
+            this.picCard2.Name = "picCard2";
+            this.picCard2.Size = new System.Drawing.Size(149, 148);
+            this.picCard2.TabIndex = 4;
+            this.picCard2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 411);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 47);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Redeal";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblHandType
+            // 
+            this.lblHandType.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHandType.Location = new System.Drawing.Point(774, 377);
+            this.lblHandType.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblHandType.Name = "lblHandType";
+            this.lblHandType.Size = new System.Drawing.Size(280, 42);
+            this.lblHandType.TabIndex = 7;
+            this.lblHandType.Text = "label2";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(281, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(149, 148);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(825, 15);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(149, 148);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(640, 15);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(149, 148);
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(460, 15);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(149, 148);
+            this.pictureBox4.TabIndex = 9;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox5.Location = new System.Drawing.Point(101, 15);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(149, 148);
+            this.pictureBox5.TabIndex = 8;
+            this.pictureBox5.TabStop = false;
+            // 
+            // Highscore
+            // 
+            this.Highscore.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+            this.Highscore.Location = new System.Drawing.Point(774, 503);
+            this.Highscore.Name = "Highscore";
+            this.Highscore.Size = new System.Drawing.Size(294, 42);
+            this.Highscore.TabIndex = 13;
+            this.Highscore.Text = "Hands won: 0";
+            // 
+            // Dlhandtype
+            // 
+            this.Dlhandtype.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+            this.Dlhandtype.Location = new System.Drawing.Point(274, 377);
+            this.Dlhandtype.Name = "Dlhandtype";
+            this.Dlhandtype.Size = new System.Drawing.Size(240, 42);
+            this.Dlhandtype.TabIndex = 15;
+            this.Dlhandtype.Text = "label3";
+            // 
+            // handwinloss
+            // 
+            this.handwinloss.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+            this.handwinloss.Location = new System.Drawing.Point(274, 484);
+            this.handwinloss.Name = "handwinloss";
+            this.handwinloss.Size = new System.Drawing.Size(240, 49);
+            this.handwinloss.TabIndex = 17;
+            this.handwinloss.Text = "label4";
+            // 
+            // Wallet
+            // 
+            this.Wallet.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+            this.Wallet.Location = new System.Drawing.Point(274, 568);
+            this.Wallet.Name = "Wallet";
+            this.Wallet.Size = new System.Drawing.Size(406, 49);
+            this.Wallet.TabIndex = 18;
+            this.Wallet.Text = "label5";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(781, 568);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(78, 21);
+            this.radioButton1.TabIndex = 19;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Bet $10";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(781, 608);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(78, 21);
+            this.radioButton2.TabIndex = 20;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Bet $50";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Bet
+            // 
+            this.Bet.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+            this.Bet.Location = new System.Drawing.Point(274, 633);
+            this.Bet.Name = "Bet";
+            this.Bet.Size = new System.Drawing.Size(306, 49);
+            this.Bet.TabIndex = 21;
+            this.Bet.Text = "Bet";
+            // 
+            // FrmPlaygame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1217, 745);
+            this.Controls.Add(this.Bet);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.Wallet);
+            this.Controls.Add(this.handwinloss);
+            this.Controls.Add(this.Dlhandtype);
+            this.Controls.Add(this.Highscore);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.lblHandType);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picCard2);
+            this.Controls.Add(this.picCard5);
+            this.Controls.Add(this.picCard4);
+            this.Controls.Add(this.picCard3);
+            this.Controls.Add(this.picCard1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmPlaygame";
+            this.Text = "FrmPlaygame";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmPlaygame_FormClosed);
+            this.Load += new System.EventHandler(this.FrmPlaygame_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picCard1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.PictureBox picCard1;
+    private System.Windows.Forms.PictureBox picCard3;
+    private System.Windows.Forms.PictureBox picCard4;
+    private System.Windows.Forms.PictureBox picCard5;
+    private System.Windows.Forms.PictureBox picCard2;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Label lblHandType;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.PictureBox pictureBox2;
+    private System.Windows.Forms.PictureBox pictureBox3;
+    private System.Windows.Forms.PictureBox pictureBox4;
+    private System.Windows.Forms.PictureBox pictureBox5;
+    private System.Windows.Forms.Label Highscore;
+    private System.Windows.Forms.Label Dlhandtype;
+    private System.Windows.Forms.Label handwinloss;
+    private System.Windows.Forms.Label Wallet;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label Bet;
+    }
+}
---
+++ Implementation/AwesomePokerGameSln/FrmPlaygame.cs
@@ -0,0 +1,112 @@
+﻿using AwesomePokerGameSln.Code;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CardType = System.Tuple<int, int>;
+
+namespace AwesomePokerGameSln {
+  public partial class FrmPlaygame : Form {
+    private Deck deck;
+    private PictureBox[] playerCardPics;
+    private PictureBox[] dealerCardPics;
+    private Hand playerHand;
+    private Hand dealerHand;
+
+    public FrmPlaygame() {
+      InitializeComponent();
+      playerCardPics = new PictureBox[5];
+      for (int c = 1; c <= 5; c++) {
+        playerCardPics[c - 1] = this.Controls.Find("picCard" + c.ToString(), true)[0] as PictureBox;
+      }
+      dealerCardPics = new PictureBox[5];
+      for (int c = 1; c <= 5; c++) {
+        dealerCardPics[c - 1] = this.Controls.Find("pictureBox" + c.ToString(), true)[0] as PictureBox;
+      }
+    }
+
+    //inserting function to check for wins and giving a default balance of $500 to player
+    int bal = 500;
+    int wins = 0;
+    int bet = 50;
+    //System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"c:\shootingstar.wav");
+        private void checkWin(HandType x, HandType y)
+        {
+            if (radioButton1.Checked) { bet = 10; }
+            if (radioButton2.Checked) { bet = 50; }
+            Bet.Text = "Bet: $" + bet;
+
+            if (x > y) { handwinloss.Text = "Dealer Wins"; bal = bal - bet; }
+            if (y > x) { handwinloss.Text = "Player Wins"; wins++; bal = bal + bet; //player.Play(); }
+            if (x == y) { handwinloss.Text = "Draw"; bal = bal + 0; }
+            Highscore.Text = "Hands won: " + wins.ToString();
+            Wallet.Text = "Wallet: $" + bal;
+        }
+
+    private void dealCards() {
+      deck.shuffleDeck();
+      Tuple<int, int>[] cards = new Tuple<int, int>[5];
+      int index = 0;
+      foreach (PictureBox playerCardPic in playerCardPics) {
+        CardType card = deck.nextCard();
+        //CardType card = new CardType(index, inde);
+        cards[index++] = card;
+        playerCardPic.BackgroundImage = CardImageHelper.cardToBitmap(card);
+      }
+      dealerHand = new Hand(cards);
+      cards = new CardType[5];
+      index = 0;
+      foreach (PictureBox dealerCardPic in dealerCardPics) {
+        CardType card = deck.nextCard();
+        //CardType card = new CardType(index, inde);
+        cards[index++] = card;
+        dealerCardPic.BackgroundImage = CardImageHelper.cardToBitmap(card);
+      }
+      playerHand = new Hand(cards);
+      lblHandType.Text = "P: " + playerHand.getHandType().ToString();
+      Dlhandtype.Text = "D: " + dealerHand.getHandType().ToString();
+            //call for checkWin function
+      checkWin(playerHand.getHandType(), dealerHand.getHandType());
+      if (bal == 0)
+            {
+                button1.Enabled = false;
+            }
+      
+      
+            //code that's probably useless now
+            /*if (playerHand.getHandType() < dealerHand.getHandType())
+            {
+                handwinloss.Text = "Player win";
+            }
+            else
+                handwinloss.Text = "Player Lose";*/
+
+    }
+
+    private void FrmPlaygame_FormClosed(object sender, FormClosedEventArgs e) {
+      foreach (Form f in Application.OpenForms)
+        f.Close();
+    }
+
+    private void FrmPlaygame_Load(object sender, EventArgs e) {
+      deck = new Deck();
+      dealCards();
+    }
+        
+    
+    private void button1_Click(object sender, EventArgs e) {
+      dealCards();
+                using (var soundPlayer = new SoundPlayer(@"C:\Users\Breon\Desktop\shootingstar.wav"))
+                {
+                    soundPlayer.Play();
+                }
+      
+            
+    }
+  }
+}
